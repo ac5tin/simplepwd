1a7cd087d8f85127606cc9d00e2ec2699b6c0c78
ac5tin <austinchang4@gmail.com>
2021-05-27 23:27:47 +0800

    change/update password

--- main.go
+++ main.go
@@ -15,12 +15,14 @@ var (
 	filename *string
 	password *string
 	dec      *bool
+	chgpw    *bool
 )
 
 func main() {
 	filename = flag.String("f", "", "File path of passwords")
 	password = flag.String("p", "", "Master password of file")
 	dec = flag.Bool("d", false, "Decrypt file and print content")
+	chgpw = flag.Bool("c", false, "Change Passowrd")
 	flag.Parse()
 
 	if useful.FileExist(*filename) {
@@ -33,11 +35,21 @@ func main() {
 			}
 			*password = string(bytePw)
 		}
+
 		data := crypto.DecryptFile(*filename, *password)
 		if *dec {
 			fmt.Println(string(*data))
 			return
 		}
+		if *chgpw {
+			fmt.Println("Please Enter New Password")
+			bytePw, err := terminal.ReadPassword(int(syscall.Stdin))
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			crypto.EncryptFile(*filename, data, string(bytePw))
+		}
+
 		repl(data)
 	} else {
 		// file doesnt exist, create file
